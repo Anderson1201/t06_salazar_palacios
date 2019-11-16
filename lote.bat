eae7cd3d4c8cd0b934ec4875546f93c9869aa8d3
Anderson1201 <57007008+anderson1201@users.noreply.github.com>
2019-11-16 02:51:01 -0500

    termine mi parte de lote.bat

---
+++ lote.bat
@@ -0,0 +1,90 @@
+python salazar/simple_01.py "Ana" 4 5.3 10 2.3
+python salazar/simple_01.py "Beatriz" 20 4.5 15 2.4
+python salazar/simple_01.py "Anabel" 12 2.3 5  2.4
+python salazar/simple_02.py "Humberto" 25 4.5
+python salazar/simple_02.py "Milagro" 10 3.3
+python salazar/simple_02.py "Flavio" 22 10.5
+python salazar/simple_03.py "Mirely" "Anderson" 12 3.5
+python salazar/simple_03.py "Mily" "Cielo" 5 8.5
+python salazar/simple_03.py "Fabian" "Miriam" 22 3.2
+python salazar/simple_04.py "Anderson" "Fernando" 14 17 18
+python salazar/simple_04.py "Pierina" "Fernando" 12 19 15
+python salazar/simple_04.py "Fabio" "Zulema" 11 13 16
+python salazar/simple_05.py "Ander" "Fernando" 15 21.5
+python salazar/simple_05.py "Holly" "Ricardo" 12 43.2
+python salazar/simple_05.py "Emely" "Lilia" 18 23.1
+python salazar/simple_06.py "Facfym" "Fernando" 12 11 14
+python salazar/simple_06.py "Fiquia" "Carlos" 13 20 23
+python salazar/simple_06.py "Fia" "Miriam" 5 4 12
+python salazar/simple_07.py 12.3 10.1 5.4
+python salazar/simple_07.py 4.5 23.4 20
+python salazar/simple_07.py 10 10 9.7
+python salazar/simple_08.py 12 10 9.5
+python salazar/simple_08.py 23.4 10 3.2
+python salazar/simple_08.py 12 23 11.9
+python salazar/simple_09.py "Mirely" "Cielo" 20 24.5 45 21.4
+python salazar/simple_09.py "Ana" "Eunice" 30 4.5 12 32.3
+python salazar/simple_09.py "Beto" "Cesia" 15 12.5 23 34.6
+python salazar/simple_10.py "Mariliza" "Grechel" 12 3.4 9 2.4
+python salazar/simple_10.py "Irma" "Maykol" 5 4.5 3 2.5
+python salazar/simple_10.py "Blanca" "Alex" 20 3.2 12 3.4
+python salazar/doble_01.py "Fernando" "Ana" 8 250.4
+python salazar/doble_01.py "Mily" "Heraclito" 12 307.8
+python salazar/doble_01.py "Anabel" "Ambrosio" 15 236
+python salazar/doble_02.py "Ana" "Fernando" 8 3.5
+python salazar/doble_02.py "Mirely" "Miriam" 3 2.4
+python salazar/doble_02.py "Yesmith" "Demetria" 20 34.5
+python salazar/doble_03.py 2 24 5.6
+python salazar/doble_03.py 2 1 45
+python salazar/doble_03.py 3 5 1
+python salazar/doble_04.py "Shugar" "Manuel" 8 12.5
+python salazar/doble_04.py "Martin" "Meco" 12 21.3
+python salazar/doble_04.py "Romero" "Raquel" 15 4.9
+python salazar/doble_05.py "Humberto" 90.7 1.87
+python salazar/doble_05.py "Pierina" 70 1.60
+python salazar/doble_05.py "Angie" 60 1.7
+python salazar/doble_06.py "Mily" "Noemi" 20 10.5
+python salazar/doble_06.py "Andres" "Miriam" 10 34
+python salazar/doble_06.py "Cesia" "Flavio" 15 20
+python salazar/doble_07.py "Ana" "Mariliza" "Misael" 5 200.5
+python salazar/doble_07.py "Bertha" "Irma" "Valdiviezo" 10 208
+python salazar/doble_07.py "Ariana" "Percy" "Miriam" 15 100.5
+python salazar/doble_08.py "Ana" "Mirta" 21 20.5
+python salazar/doble_08.py "Mercy" "Javier" 22 12.4
+python salazar/doble_08.py "Raquel" "Victor" 30 13.8
+python salazar/doble_09.py "Ander" 12 2.4 11 3.5
+python salazar/doble_09.py "Rafael" 9 3.5 4 2.1
+python salazar/doble_09.py "Fernon" 15 3.2 18 3.2
+python salazar/doble_10.py "Diana" 10 4.5
+python salazar/doble_10.py "Dangelo" 30 2.2
+python salazar/doble_10.py "Gabriel" 12 5.2
+python salazar/multiple_1.py "Ander" 12 2 8 2.3
+python salazar/multiple_1.py "Mirta" 10 3 10 3
+python salazar/multiple_1.py "Juan" 4 2.3 6 5.4
+python salazar/multiple_2.py "Pedro" 21 4.5
+python salazar/multiple_2.py "Juan" 16 2.4
+python salazar/multiple_2.py "Anita" 12 3.2
+python salazar/multiple_3.py "Mily" "Meche" 15 3.4
+python salazar/multiple_3.py "Anabel" "Jimena" 12 21.2
+python salazar/multiple_3.py "Angie" "Nayely" 38 13.6
+python salazar/multiple_4.py "Mirely" "Miriam" 14 16 20
+python salazar/multiple_4.py "Fernando" "Cilo" 15 12 11
+python salazar/multiple_4.py "Maykol" "Maricielo" 11 10 17
+python salazar/multiple_05.py "Teresa" "Alberto" 15 14.2
+python salazar/multiple_05.py "Juana" "Moises" 12 20
+python salazar/multiple_05.py "Kyara" "Aurelio" 20 3.6
+python salazar/multiple_6.py "Facfym" "Lara" 10 2 3
+python salazar/multiple_6.py "Fiquia" "Alberto" 2 15 17
+python salazar/multiple_6.py "Fime" "Augusto" 5 6 10
+python salazar/multiple_7.py 10 20 5
+python salazar/multiple_7.py 5 6 10
+python salazar/multiple_7.py 3.5 6 15
+python salazar/multiple_8.py 10 2 8
+python salazar/multiple_8.py 10 23 1
+python salazar/multiple_8.py 12 13 4
+python salazar/multiple_9.py "ana" "bertha" 23 4.5 12 4.5
+python salazar/multiple_9.py "Aleja" "Diego" 5 3 12 3.5
+python salazar/multiple_9.py "Alex" "Paul" 12 33 4 2
+python salazar/multiple_10.py "Rafa" "Bertina" 12 4.5 11 9.6
+python salazar/multiple_10.py "Ana" "Celia" 12 34 5 4
+python salazar/multiple_10.py "Cesi" "Jossely" 10 2.4 23 4.5
